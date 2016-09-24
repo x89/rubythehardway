35ebdc8ad893900ca8b25ce8e7eee6ec24f3f96b
Naypam <github@vaunt.eu>
2016-09-24 19:46:39 +0100

    Loops loops loops

--- codewars/coffee-rlog.rb
+++ codewars/coffee-rlog.rb
@@ -18,3 +18,33 @@ events.each { |event|
 if events.include? "cw"
   puts "1 coffee"
 end
+
+
+def how_much_coffee(events)
+  count = 0
+
+  events.each do |event|
+    if event.include? "cw"
+      count += 1
+    elsif event == "CW"
+      count += 2
+    elsif event.include? "cat"
+      count += 1
+    elsif event.include? "CAT"
+      count += 2
+    elsif event.include? "dog"
+      count += 1
+    elsif event.include? "DOG"
+      count += 2
+    end
+  end
+
+  if count > 3
+    "You need extra sleep"
+  else
+    puts "You need #{count} coffees"
+    return count
+  end
+end
+
+puts how_much_coffee(events)
