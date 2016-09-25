f29aa8a6eb61b2d2a9f8cf69c9f52d8f313a585e
Naypam <github@vaunt.eu>
2016-09-25 11:53:22 +0100

    Test suite

--- codewars/trailing_zeroes.rb
+++ codewars/trailing_zeroes.rb
@@ -1,3 +1,5 @@
+require 'test/unit'
+
 class Integer
   def fac
     (1..self).reduce(1, :*)
@@ -12,8 +14,26 @@ class Integer
     end
     return count
   end
+
+  def trailing_zeroes_fast
+    
+  end
 end
 
-(1..1000).each { |x| puts "#{x}: #{x.fac.trailing_zeroes}" }
+#(1..1000).each { |x| puts "#{x}: #{x.fac.trailing_zeroes}" }
 
-#assert_equal(trailing_zeroes, 1)
+class TestTrailingZeroes < Test::Unit::TestCase
+  def test_original
+    # Test original trailing_zeroes
+    assert_equal(1, 5.fac.trailing_zeroes)
+    assert_equal(2, 12.fac.trailing_zeroes)
+    assert_equal(1, 0.fac)
+  end
+
+  def test_fast
+    # Test "fast" trailing_zeroes
+    assert_equal(1, 5.fac.trailing_zeroes_fast)
+    assert_equal(2, 12.fac.trailing_zeroes_fast)
+    assert_equal(24, 100.fac.trailing_zeroes_fast)
+  end
+end
