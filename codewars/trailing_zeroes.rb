9a6b637b6fd969e957e93829382ae59c8a5f3b8c
Naypam <github@vaunt.eu>
2016-09-25 12:13:13 +0100

    Fast trailing_zeroes function \o/

--- codewars/trailing_zeroes.rb
+++ codewars/trailing_zeroes.rb
@@ -16,24 +16,34 @@ class Integer
   end
 
   def trailing_zeroes_fast
-    
+    '''Trailing zeroes goes up n/5 every n'''
+    n = self
+    count = 0
+    count += n /= 5 while n > 4
+    return count
   end
 end
 
-#(1..1000).each { |x| puts "#{x}: #{x.fac.trailing_zeroes}" }
+# (1000000..1000001).each { |x| puts "#{x}: #{x.trailing_zeroes_fast}" }
 
 class TestTrailingZeroes < Test::Unit::TestCase
   def test_original
     # Test original trailing_zeroes
     assert_equal(1, 5.fac.trailing_zeroes)
     assert_equal(2, 12.fac.trailing_zeroes)
+    assert_equal(12, 50.fac.trailing_zeroes)
+    assert_equal(24, 100.fac.trailing_zeroes)
     assert_equal(1, 0.fac)
   end
 
   def test_fast
     # Test "fast" trailing_zeroes
-    assert_equal(1, 5.fac.trailing_zeroes_fast)
-    assert_equal(2, 12.fac.trailing_zeroes_fast)
-    assert_equal(24, 100.fac.trailing_zeroes_fast)
+    assert_equal(1, 5.trailing_zeroes_fast)
+    assert_equal(2, 12.trailing_zeroes_fast)
+    assert_equal(12, 50.trailing_zeroes_fast)
+    assert_equal(24, 100.trailing_zeroes_fast)
+    assert_equal(500.fac.trailing_zeroes, 500.trailing_zeroes_fast)
+    assert_equal(2000.fac.trailing_zeroes, 2000.trailing_zeroes_fast)
+    assert_equal(249999998, 1000000000.trailing_zeroes_fast)
   end
 end
