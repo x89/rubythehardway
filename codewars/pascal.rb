7196042f0d91113b2d589fe02a128fdde837e106
David Trail <david@vaunt.eu>
2016-09-29 11:51:14 +0100

    Fucking rounding errors! 461.99999999999994

--- codewars/pascal.rb
+++ codewars/pascal.rb
@@ -5,16 +5,22 @@ class Integer
 
   def pascal_row
     tmp = 1
-    row = Array.new [1.0]
+    row = Array.new [1]
     self.downto(1).each do |n|
-      fraction = n/(self-(n-1)).to_f  # 5/1, 4/2, 3/3, 2/4, 1/5…
-      #puts "#{n}: #{n}/#{self-(n-1)} = #{fraction}"
-      row.push(tmp *= fraction)
+      fraction = n/(self-(n-1)).to_f.round(10)  # 5/1, 4/2, 3/3, 2/4, 1/5…
+      puts "#{n}: #{n}/#{self-(n-1)} = #{fraction} => #{(fraction * tmp).round}"
+      row.push((tmp *= fraction).round)
     end
     return row.map{|e| e.to_i}
   end
+
+  def pascal_flat_row
+    a = Array.new()
+    (0..self).each do |n|
+      a.push(n.pascal_row)
+    end
+    return a.flatten
+  end
 end
 
-(0..10).each do |n|
-  puts "#{n.pascal_row}"
-end
+(11..11).each {|x| puts "#{x.pascal_row}" }
