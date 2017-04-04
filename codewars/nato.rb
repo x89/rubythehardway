60352eb3fc69d63a02c9fc15656b72ca736904b1
David <github@vaunt.eu>
2017-04-04 10:03:05 +0100

    Done, unmentioned NATO dict, I want my 5 minutes back.

--- codewars/nato.rb
+++ codewars/nato.rb
@@ -1,10 +1,17 @@
+# Note for people of the future, there's a NATO dict in the ruby namespace
+
 DICT = {
-  'a' => 'Alfa',
-  'b' => 'Bravo',
-  'c' => 'Charlie', 'd' => 'Delta', 'e' => 'Echo', 'f' => 'Foxtrot',
-  'g' => 'Golf', 'h' => 'Hotel', 'i' => 'India', 'j' => 'Juliett',
-  'k' => 'Kilo', 'l' => 'Lima', 'm' => 'Mike', 'n' => 'November',
-  'o' => 'Oscar', 'p' => 'Papa', 'q' => 'Quebec', 'r' => 'Romeo',
-  's' => 'Sierra', 't' => 'Tango', 'u' => 'Uniform', 'v' => 'Victor',
-  'w' => 'Whiskey', 'x' => 'Xray', 'y' => 'Yankee', 'z' => 'Zulu'
+  'a' => 'Alfa', 'b' => 'Bravo', 'c' => 'Charlie', 'd' => 'Delta',
+  'e' => 'Echo', 'f' => 'Foxtrot', 'g' => 'Golf', 'h' => 'Hotel',
+  'i' => 'India', 'j' => 'Juliett', 'k' => 'Kilo', 'l' => 'Lima', 'm' => 'Mike',
+  'n' => 'November', 'o' => 'Oscar', 'p' => 'Papa', 'q' => 'Quebec',
+  'r' => 'Romeo', 's' => 'Sierra', 't' => 'Tango', 'u' => 'Uniform',
+  'v' => 'Victor', 'w' => 'Whiskey', 'x' => 'Xray', 'y' => 'Yankee',
+  'z' => 'Zulu', '!' => '!', '?' => '?', '.' => '.'
 }
+
+def to_nato(words)
+  words.delete(' ').chars.each.map { |c| DICT[c.downcase] }.join(' ')
+end
+
+puts to_nato("Just another Ruby hacker!")
