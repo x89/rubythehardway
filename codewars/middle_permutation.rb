eb49c0e1a1ece30e3f39ce8c9b930ecd0074dcd9
David John <github@vaunt.eu>
2017-04-12 19:07:34 +0100

    Clever(ish) solution, at least a non-bruteforce

--- codewars/middle_permutation.rb
+++ codewars/middle_permutation.rb
@@ -1,16 +1,15 @@
-def fac(n)
-  # n! is max for s.length
-  (1..n).reduce(1, :*)
+class String
+  def even?
+    self.length % 2 == 0
+  end
 end
 
 def middle_permutation(string)
-  arr = Array.new
-  max = fac(string.length)
-  idx = max % 2 == 0 ? max/2-1 : max/2
-  string.chars.permutation.each_with_index do |s, i|
-    arr.push(s)
-    break if i + 1 > max
+  string = string.chars.sort.join
+  mid = string.length / 2 - 1
+  if string.even?
+    string[mid] + (string.slice(0, mid) + string.slice(mid+1, string.length)).reverse
+  else
+    string.slice(mid, 2).reverse + (string.slice(0, mid) + string.slice(mid+2, string.length)).reverse
   end
-  arr.sort!
-  arr[idx].join()
 end
