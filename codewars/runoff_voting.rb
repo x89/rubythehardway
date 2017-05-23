7b215142436bca2e7e9bd28d2d7ac4ca492cb12e
David John <github@vaunt.eu>
2017-05-23 15:37:09 +0100

    Another test, debug statements and some improved logic

--- codewars/runoff_voting.rb
+++ codewars/runoff_voting.rb
@@ -17,6 +17,7 @@ end
 
 def least_voted_candidate(votes)
   '''Return the candidate with the least votes.'''
+  votes.first_row.min_by { |x| votes.first_row.count }
 end
 
 def winner(votes)
@@ -34,12 +35,18 @@ def runoff(votes)
   '''Takes an Array of Arrays in descending order of preference of candidates,
   returns the winning candidate via an AV style runnoff vote system (similar
   to Scottish council elections).'''
-  votes.each_with_index do |vote, idx|
-    if w = winner(votes.first_row)
-      return w
+  while not w = winner(votes.first_row)
+    first_row = votes.first_row
+    least_voted = least_voted_candidate(votes).to_sym
+    puts "Found #{least_voted} in #{first_row}"
+    votes.each do |vote|
+      old_vote = vote.dup
+      vote.delete least_voted
+      puts "Deleted #{least_voted} from #{old_vote} to make #{vote}"
     end
-    puts "#{least_voted_candidate(votes.last_row)}"
   end
+  #puts "#{votes}"
+  return w
 end
 
 voters = [[:dem, :ind, :rep],
@@ -61,3 +68,10 @@ voters = [[:d, :a, :c, :b, :e],
           [:c, :d, :e, :a, :b],
           [:c, :e, :b, :a, :d]]
 Test.assert_equals(runoff(voters), :c)
+
+voters = [[:e, :c, :d, :b, :a],
+          [:b, :e, :a, :c, :d],
+          [:c, :e, :d, :a, :b],
+          [:a, :d, :e, :b, :c],
+          [:e, :d, :c, :a, :b]]
+Test.assert_equals(runoff(voters), :e)
