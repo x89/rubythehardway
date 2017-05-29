6f1aaf13dbcc69276253ffff91a23fb3d079e6c3
David John <github@vaunt.eu>
2017-05-29 13:41:39 +0100

    Brute force done! 13.33s to do up to 10 chars

--- codewars/alphabetic_anagrams.rb
+++ codewars/alphabetic_anagrams.rb
@@ -6,15 +6,31 @@ Calculate the position in a sorted list of strings matching /[A-Z]{,25}/.
 
 require './test-framework/framework.rb'
 
+def slow_mode(word)
+  word.chars.permutation.uniq.sort
+end
+
 def listPosition(word)
-    #your code here
+    puts "Running for word: #{word}"
+    slow_mode(word).each_with_index do |w, idx|
+      #puts "#{idx+1}: #{w.join('')}"
+      return idx+1 if word == w.join('')
+    end
 end
 
 Test.describe('Anagram') do
   Test.it('Must return appropriate values for known inputs') do
-    testValues = {'A' => 1, 'ABAB' => 2, 'AAAB' => 1, 'BAAA' => 4, 'QUESTION' => 24572, 'BOOKKEEPER' => 10743}
+    testValues = {
+      'A' => 1,
+      'ABAB' => 2,
+      'AAAB' => 1,
+      'BAAA' => 4,
+      'QUESTION' => 24572,
+      'BOOKKEEPER' => 10743
+    }
     testValues.each do |key,value|
-      Test.assert_equals(listPosition(key), value, 'Incorrect list position for: ' + key)
+      Test.assert_equals(listPosition(key),
+        value, 'Incorrect list position for: ' + key)
     end
   end
 end
